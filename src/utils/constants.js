9356576b48b8ab9fb736d52f00c736ecb705db67
qinanagao@gmail.com <qinanagao@gmail.com>
2025-07-11 16:17:39 +0100

    add validation for email and password

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -2,4 +2,9 @@ export const fieldValidation = {
   title: { minLength: 3, maxLength: 60, required: true },
   address: { minLength: 3, maxLength: 60, required: true },
   description: { minLength: 10, maxLength: 150, required: true },
+  password: { minLength: 5, maxLength: 20, required: true },
+  email: {
+    required: true,
+    pattern: /^[^\s@]+@[^\s@]+\.[^\s@]+$/,
+  },
 };
