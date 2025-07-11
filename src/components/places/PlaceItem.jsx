a01b11f13c9318b6f9e70c96a54201ec32cf1a48
qinanagao@gmail.com <qinanagao@gmail.com>
2025-07-11 16:16:45 +0100

    add delete place modal

--- src/components/places/PlaceItem.jsx
+++ src/components/places/PlaceItem.jsx
@@ -14,6 +14,12 @@ function PlaceItem({
   description,
 }) {
   const [isMapOpen, setIsMapOpen] = useState(false);
+  const [isDelete, setIsDelete] = useState(false);
+
+  function handleDelete() {
+    console.log("delete");
+    setIsDelete(false);
+  }
 
   return (
     <>
@@ -48,19 +54,55 @@ function PlaceItem({
               <Link to={`/places/${id}`}>
                 <Button variant="primary">Edit</Button>
               </Link>
-              <Button variant="default">Delete</Button>
+              <Button variant="default" onClick={() => setIsDelete(true)}>
+                Delete
+              </Button>
             </div>
           </div>
         </div>
       </li>
 
-      <Modal
-        isOpen={isMapOpen}
-        onClose={() => setIsMapOpen(false)}
-        title={address}
-      >
-        <div className="w-full h-64 relative">
+      <Modal isOpen={isMapOpen}>
+        <div className="flex items-center justify-between pb-5 border-b">
+          <h3 className="text-lg font-semibold text-gray-900">{address}</h3>
+          <button
+            onClick={() => setIsMapOpen(false)}
+            className="text-gray-400 hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 flex items-center justify-center"
+          >
+            <svg
+              className="w-3 h-3"
+              fill="none"
+              viewBox="0 0 14 14"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                stroke="currentColor"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M1 1l6 6m0 0l6 6M7 7l6-6M7 7L1 13"
+              />
+            </svg>
+            <span className="sr-only">Close modal</span>
+          </button>
+        </div>
+        <div className="h-64 w-full relative">
           <Map location={location} />
+        </div>
+      </Modal>
+
+      <Modal isOpen={isDelete}>
+        <p className="text-gray-800 mb-5 mt-2">
+          Are you sure you want to delete this place? Please notre that it can
+          not be undone thereafter.
+        </p>
+        <div className="flex justify-end gap-2 mb-2">
+          <Button variant="default" onClick={() => setIsDelete(false)}>
+            Cancel
+          </Button>
+          <Button variant="primary" onClick={handleDelete}>
+            Delete
+          </Button>
         </div>
       </Modal>
     </>
