113dd79ea7e4ccd529b12ac19cc934fdd2e40ab4
qinanagao@gmail.com <qinanagao@gmail.com>
2025-07-11 20:28:28 +0100

    add valdaiton for name

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -2,6 +2,7 @@ export const fieldValidation = {
   title: { minLength: 3, maxLength: 60, required: true },
   address: { minLength: 3, maxLength: 60, required: true },
   description: { minLength: 10, maxLength: 150, required: true },
+  username: { minLength: 1, maxLength: 20, required: true },
   password: { minLength: 5, maxLength: 20, required: true },
   email: {
     required: true,
