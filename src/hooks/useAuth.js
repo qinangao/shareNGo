a05d2e58893378396da10e7d317da8dfd2af1146
qinanagao@gmail.com <qinanagao@gmail.com>
2025-07-12 20:33:16 +0100

    add useAuth to listen to login state

---
+++ src/hooks/useAuth.js
@@ -0,0 +1,25 @@
+import { createContext, useContext, useState } from "react";
+
+const AuthContext = createContext();
+
+function AuthContextProvider({ children }) {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  const login = () => setIsLoggedIn(true);
+  const logout = () => setIsLoggedIn(false);
+
+  return (
+    <AuthContext.Provider value={{ isLoggedIn, login, logout }}>
+      {children}
+    </AuthContext.Provider>
+  );
+}
+
+function useAuth() {
+  const context = useContext(AuthContext);
+  if (context === undefined)
+    throw new Error("AuthContext was used outside the AuthProvider");
+  return context;
+}
+
+export { AuthContextProvider, useAuth };
