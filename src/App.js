a85223d54d938097a289f4b376780c6734f77d67
qinanagao@gmail.com <qinanagao@gmail.com>
2025-07-12 20:31:53 +0100

    wrap route in public and protected route

--- src/App.js
+++ src/App.js
@@ -1,31 +1,69 @@
 import { createBrowserRouter, Navigate, RouterProvider } from "react-router";
 import Applayout from "./Applayout";
 import User from "./pages/User";
-import Places from "./pages/Places";
 import UserPlaces from "./pages/UserPlaces";
 import NewPlace from "./components/places/NewPlace";
 import UpdatePlace from "./pages/UpdatePlace";
 import Auth from "./pages/Auth";
+import { AuthContextProvider } from "./hooks/useAuth";
+import ProtectedRoute from "./routes/ProtectedRoute";
+import PublicRoute from "./routes/PublicRoute";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Applayout />,
     children: [
-      // { index: true, element: <Home /> },
-      { path: "/auth", element: <Auth /> },
-      { path: "/user", element: <User /> },
-      { path: "/:userId/places", element: <UserPlaces /> },
-      { path: "/places", element: <Places /> },
-      { path: "/places/new", element: <NewPlace /> },
-      { path: "/places/:placeId", element: <UpdatePlace /> },
+      // Optional: redirect based on login state
+      { index: true, element: <User /> },
+
+      // Public pages
+      {
+        path: "/auth",
+        element: (
+          <PublicRoute>
+            <Auth />
+          </PublicRoute>
+        ),
+      },
+      {
+        path: "/:userId/places",
+        element: <UserPlaces />,
+      },
+      {
+        path: "/user",
+        element: <User />,
+      },
+
+      // Protected pages
+      {
+        path: "/places/new",
+        element: (
+          <ProtectedRoute>
+            <NewPlace />
+          </ProtectedRoute>
+        ),
+      },
+      {
+        path: "/places/:placeId",
+        element: (
+          <ProtectedRoute>
+            <UpdatePlace />
+          </ProtectedRoute>
+        ),
+      },
+
       { path: "*", element: <Navigate to="/" replace /> },
     ],
   },
 ]);
 
 function App() {
-  return <RouterProvider router={router} />;
+  return (
+    <AuthContextProvider>
+      <RouterProvider router={router} />
+    </AuthContextProvider>
+  );
 }
 
 export default App;
