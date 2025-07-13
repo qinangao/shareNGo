3813634887e62f600a9ec29660a77ed452846232
qinanagao@gmail.com <qinanagao@gmail.com>
2025-07-13 16:26:08 +0100

    add colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,24 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        brand: {
+          50: "#E3F7FE",
+          100: "#ade7fe",
+          200: "#94DFFE",
+          300: "#679CB1",
+        },
+        red: { 100: "#dc3545", 200: "#bd2130" },
+        light: {
+          100: "#FEF5E8",
+        },
+        dark: {
+          50: "#545b6e",
+          100: "#333743",
+        },
+      },
+    },
   },
   plugins: [],
 };
