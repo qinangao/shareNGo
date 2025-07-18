745162375359fdab678467a2a9ed6e765def0117
qinanagao@gmail.com <qinanagao@gmail.com>
2025-07-18 15:14:46 +0100

    move your context into its own file

---
+++ src/hooks/AuthContextProvider.jsx
@@ -0,0 +1,15 @@
+import { useState } from "react";
+import { AuthContext } from "./AuthContext";
+
+export function AuthContextProvider({ children }) {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  const login = () => setIsLoggedIn(true);
+  const logout = () => setIsLoggedIn(false);
+
+  return (
+    <AuthContext.Provider value={{ isLoggedIn, login, logout }}>
+      {children}
+    </AuthContext.Provider>
+  );
+}
