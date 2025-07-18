74ef11c7d33c1a0a0b051ad89c30174101459012
qinanagao@gmail.com <qinanagao@gmail.com>
2025-07-18 14:59:48 +0100

    add error modal

---
+++ src/components/ErrorModal.jsx
@@ -0,0 +1,29 @@
+// components/ErrorModal.jsx
+import {
+  Dialog,
+  DialogContent,
+  DialogHeader,
+  DialogTitle,
+  DialogFooter,
+  DialogClose,
+  DialogDescription,
+} from "@/components/ui/dialog";
+import { Button } from "@/components/ui/button";
+
+export default function ErrorModal({ open, message, onClose }) {
+  return (
+    <Dialog open={open} onOpenChange={onClose}>
+      <DialogContent>
+        <DialogHeader>
+          <DialogTitle className="text-red-600">Error</DialogTitle>
+        </DialogHeader>
+        <DialogDescription>{message}</DialogDescription>
+        <DialogFooter>
+          <DialogClose asChild>
+            <Button onClick={onClose}>OK</Button>
+          </DialogClose>
+        </DialogFooter>
+      </DialogContent>
+    </Dialog>
+  );
+}
