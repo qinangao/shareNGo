326608b9950fe4c07b3f11fce09533ac9757f64d
qinanagao@gmail.com <qinanagao@gmail.com>
2025-07-19 19:37:38 +0100

    fetch currentuser places

--- src/pages/UserPlaces.jsx
+++ src/pages/UserPlaces.jsx
@@ -1,39 +1,46 @@
 import { useParams } from "react-router";
 import PlaceList from "../components/places/PlaceList";
-
-const DUMMY_PLACES = [
-  {
-    id: "p1",
-    imageUrl:
-      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSzMnNyQjI5LDUexeUDV92SXnYSISARRYsMUKZmZPlwFEK_-BjeXbEchorS41RugzL7X2k&usqp=CAU",
-    title: "South Park",
-    address: "Park Vale Rd, Macclesfield SK11 8AD",
-    description:
-      "South Park is a friendly neighbourhood in Macclesfield, England, known for its community spirit and walkability.",
-    creator: "u1",
-    location: { lat: 53.2509364, lng: -2.1320182 },
-  },
-  {
-    id: "p2",
-    imageUrl:
-      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSzMnNyQjI5LDUexeUDV92SXnYSISARRYsMUKZmZPlwFEK_-BjeXbEchorS41RugzL7X2k&usqp=CAU",
-    title: "West Park",
-    address: "Park Vale Rd, Macclesfield SK11 8AD",
-    description:
-      "South Park is a friendly neighbourhood in Macclesfield, England, known for its community spirit and walkability.",
-    creator: "u2",
-    location: { lat: 53.2509364, lng: -2.1320182 },
-  },
-];
+import useHttp from "@/hooks/useHttp";
+import { useEffect, useState } from "react";
+import ErrorModal from "@/components/ErrorModal";
+import { Spinner } from "@/components/Spinner";
 
 function UserPlaces() {
   const userId = useParams().userId;
+  const [userPlaces, setUserPlaces] = useState();
+  const { isLoading, errorMessage, errorModalOpen, sendRequest, clearError } =
+    useHttp();
 
-  const currentUserPlaces = DUMMY_PLACES.filter(
-    (place) => place.creator === userId
+  const endpoint = `http://localhost:5000/api/places/user/${userId}`;
+
+  useEffect(() => {
+    async function fetchPlaces() {
+      try {
+        const data = await sendRequest(endpoint);
+
+        if (data && data.userPlaces) {
+          setUserPlaces(data.userPlaces);
+        }
+      } catch (error) {
+        console.error(error.message);
+      }
+    }
+
+    fetchPlaces();
+  }, [sendRequest, endpoint]);
+
+  // console.log(userPlaces);
+  return (
+    <>
+      {isLoading && <Spinner />}
+      {!isLoading && userPlaces && <PlaceList places={userPlaces} />}
+      <ErrorModal
+        open={errorModalOpen}
+        message={errorMessage}
+        onClose={clearError}
+      />
+    </>
   );
-
-  return <PlaceList places={currentUserPlaces} />;
 }
 
 export default UserPlaces;
