c98cedc3f4db85dccd5626c9b8f7ae95b7e3ffbf
qinanagao@gmail.com <qinanagao@gmail.com>
2025-07-19 19:36:56 +0100

    add useId in useAuth hook

--- src/hooks/AuthContextProvider.jsx
+++ src/hooks/AuthContextProvider.jsx
@@ -1,14 +1,21 @@
-import { useState } from "react";
+import { useCallback, useState } from "react";
 import { AuthContext } from "./AuthContext";
 
 export function AuthContextProvider({ children }) {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [userId, setUserId] = useState(null);
 
-  const login = () => setIsLoggedIn(true);
-  const logout = () => setIsLoggedIn(false);
+  const login = useCallback((userId) => {
+    setIsLoggedIn(true);
+    setUserId(userId);
+  }, []);
+  const logout = useCallback(() => {
+    setIsLoggedIn(false);
+    setUserId(null);
+  }, []);
 
   return (
-    <AuthContext.Provider value={{ isLoggedIn, login, logout }}>
+    <AuthContext.Provider value={{ isLoggedIn, login, logout, userId }}>
       {children}
     </AuthContext.Provider>
   );
