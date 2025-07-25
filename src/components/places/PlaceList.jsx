82ff657a11bbb09c3e93a63bbc0303313a1fb4ee
qinanagao@gmail.com <qinanagao@gmail.com>
2025-07-25 10:31:31 +0100

    refactor placelist

--- src/components/places/PlaceList.jsx
+++ src/components/places/PlaceList.jsx
@@ -1,16 +1,30 @@
 import { Button } from "@/components/ui/button";
 import PlaceItem from "./PlaceItem";
 import { Link } from "react-router";
+import LinkButton from "../LinkButton";
+import { ArrowLeft } from "lucide-react";
 
 function PlaceList({ places, onDelete, isCurrentUser = false }) {
-  if (!places || places.length === 0) {
-    return (
-      <div className="error_container">
-        <div className="text-center">
-          <h2 className="text-xl font-semibold text-gray-600 mb-[30px]">
+  const hasPlaces = places && places.length > 0;
+
+  return (
+    <div className="container">
+      <h1 className="title">{isCurrentUser ? "Your Places" : "Places"}</h1>
+
+      {/* Back to Community link (always shown) */}
+      <Link to="/user">
+        <LinkButton className="flex items-center my-8">
+          <ArrowLeft />
+          <span className="ml-2">Back to Community</span>
+        </LinkButton>
+      </Link>
+
+      {!hasPlaces ? (
+        <div className="error_container text-center">
+          <h2 className="text-xl font-semibold text-gray-600 mb-8">
             {isCurrentUser
-              ? "You haven't posted any place yet"
-              : "This user hasn't posted any place"}
+              ? "You haven't posted any places yet."
+              : "This user hasn't posted any places."}
           </h2>
           {isCurrentUser && (
             <Link to="/places/new">
@@ -18,28 +32,23 @@ function PlaceList({ places, onDelete, isCurrentUser = false }) {
             </Link>
           )}
         </div>
-      </div>
-    );
-  }
-
-  return (
-    <div className="container">
-      <h1 className="title">{isCurrentUser ? "Your Places" : "Places"}</h1>
-      <ul className="grid grid-cols-1 lg:grid-cols-2 2xl:grid-cols-3 gap-6 mt-[50px]">
-        {places.map((place) => (
-          <PlaceItem
-            key={place.id}
-            id={place.id}
-            image={place.image}
-            title={place.title}
-            address={place.address}
-            description={place.description}
-            creatorId={place.creator}
-            location={place.location}
-            onDelete={onDelete}
-          />
-        ))}
-      </ul>
+      ) : (
+        <ul className="grid grid-cols-1 lg:grid-cols-2 2xl:grid-cols-3 gap-6">
+          {places.map((place) => (
+            <PlaceItem
+              key={place.id}
+              id={place.id}
+              image={place.image}
+              title={place.title}
+              address={place.address}
+              description={place.description}
+              creatorId={place.creator}
+              location={place.location}
+              onDelete={onDelete}
+            />
+          ))}
+        </ul>
+      )}
     </div>
   );
 }
